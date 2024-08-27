19f472bf55ecc40e129a58bb1d088bc145a78110
Professora Glau <80214705+glaucimara-jpg@users.noreply.github.com>
2024-08-27 15:37:10 -0300

    Update condicionais.py

--- condicionais.py
+++ condicionais.py
@@ -1,20 +1,48 @@
-# Enquanto determinada condição for verdadeira, o processo se repete.
 
-x = 0
 
-while x <= 5:
-  print(x)
-  x = x + 1
-  
-# Enquanto o usuário não digitar a palavra específica o código não é executado.
+num = 0
+total = 10
 
-validador = input('Digite "Brasil" para continuar:')
+while num <= 10:
+  # Enquanto o valor de num for igual ou menor que 10
+  print(num)
+  num += 1
+  # num é atualizado, incrementado em 1 unidade
+  if num == 5:
+    # Se o valor de num for igual a 5
+    print('50% computado')
+  if num == 10:
+    # Se o valor de num for igual a 10
+    print('100%, processo encerrado')
 
-while validador != 'Brasil':
-    print('Palavra-chave não confere, digite novamente:')
-    validador = input('Digite "Brasil" para continuar:')
 
+
+
+
+    while True:
+       
+     validador = input('Digite "Brasil" para continuar:')
+    
     if validador == 'Brasil':
-        print('Agora você digitou Brasil corretamente.')
+        print('Você digitou Brasil corretamente!!!')
+        break # interrompe o processo / loop
+    else:
+        print('Palavra-chave não confere, digite novamente:')
 
-     
+# while True nesse caso simula um ciclo "infinito", sendo necessário que quando
+# a condição for atingida, o marcador break interrompe o ciclo.
+
+
+
+
+
+
+# Laço de repetição simples
+
+compras = ['Arroz', 'Feijão', 'Massa', 'Carne', 'Pão']
+
+for i in compras:
+  print(i)
+
+# A variável temporária i a cada ciclo irá ler um dos elementos da lista
+# compras, sendo que a cada ciclo lê e retorna o elemento subsequente.
